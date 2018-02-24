e59a6acee721e6feda6704526906e8f603572b7d
xtzaferis <xtzaferis@gmail.com>
2018-02-24 10:35:43 +0100

    navbar added

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent],
+    declarations: [
+        EventsAppComponent, 
+        EventsListComponent, 
+        EventThumbnailComponent,
+        NavBarComponent
+    ],
     bootstrap: [EventsAppComponent]
 })
 
